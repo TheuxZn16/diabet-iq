3d6b2108ec0e145bbbf9cfd16f06d4bd2c7c6fd0
TheuxZn16 <matheusestevam421@gmail.com>
2026-07-23 15:08:40 -0300

    init

---
+++ src/main/java/theuxzn16/com/github/diabetiq/DiabetIqApplication.java
@@ -0,0 +1,13 @@
+package theuxzn16.com.github.diabetiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabetIqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiabetIqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/theuxzn16/com/github/diabetiq/DiabetIqApplicationTests.java
@@ -0,0 +1,13 @@
+package theuxzn16.com.github.diabetiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabetIqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
