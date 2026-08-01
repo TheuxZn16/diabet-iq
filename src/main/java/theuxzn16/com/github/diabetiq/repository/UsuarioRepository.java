7094517388de5992c28bdcd6d55554bac3f3d4c0
TheuxZn16 <matheusestevam421@gmail.com>
2026-08-01 13:01:09 -0300

    feature: criado sistema de cadastro de um novo usuario medico

--- src/main/java/theuxzn16/com/github/diabetiq/repository/UsuarioRepository.java
+++ src/main/java/theuxzn16/com/github/diabetiq/repository/UsuarioRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import theuxzn16.com.github.diabetiq.entity.Usuario;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+    Boolean existsByEmail(String email);
 }
--- src/main/java/theuxzn16/com/github/diabetiq/security/SecurityConfig.java
+++ src/main/java/theuxzn16/com/github/diabetiq/security/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
                         auth
-                                .requestMatchers(HttpMethod.POST, "/api/v1/user", "/api/v1/auth").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/v1/usuario/medico", "/api/v1/usuario/paciente", "/api/v1/auth").permitAll()
                                 .requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**").permitAll()
                                 .anyRequest().authenticated()
                 );
